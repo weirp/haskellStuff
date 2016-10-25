1935889bf2a3ae248b289a9a362b58ce86461884
Philip J Weir <weirp@acm.org>
2016-10-26 06:40:02 +0800

    applicatives

--- InteractiveDOubling.hs
+++ InteractiveDOubling.hs
@@ -9,3 +9,22 @@ interactiveDoubling = do
     Nothing -> do
       putStrLn "This is not a valid number. Retrying ..."
       interactiveDoubling
+
+interactiveSumming = do
+  sa <- getLine
+  sb <- getLine
+  let ma = readMaybe sa :: Maybe Double
+      mb = readMaybe sb :: Maybe Double
+  case ma of
+    Just a -> case mb of
+      Just b -> putStrLn ("The sum is " ++ show (a + b))
+      Nothing -> retry
+    Nothing -> retry
+  where retry = do
+          putStrLn "Invalid number"
+          interactiveSumming
+
+{- at ghci need to import Control.Applicative
+to get result from:
+:t (<*>)
+-}
