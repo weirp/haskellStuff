4cd00028d19e3b48a46ebe7cd965fcec8dd4adf1
Philip J Weir <weirp@acm.org>
2016-11-20 07:18:25 +0800

    localMaxima

--- cis194/Golf.hs
+++ cis194/Golf.hs
@@ -16,3 +16,26 @@ skipsHelper lst n
   | (length lst) >= n =
       (head (take 1 (drop (n - 1) lst))) : (skipsHelper (drop n lst) n)
   | otherwise = []
+
+{-
+Exercise 2 Local maxima
+A local maximum of a list is an element of the list which is strictly
+greater than both the elements immediately before and after it. For
+example, in the list [2,3,4,1,5], the only local maximum is 4, since
+it is greater than the elements immediately before and after it (3 and
+1). 5 is not a local maximum since there is no element that comes
+after it.
+Write a function
+localMaxima :: [Integer] -> [Integer]
+which finds all the local maxima in the input list and returns them in
+order. For example:
+localMaxima [2,9,5,6,1] == [9,6]
+localMaxima [2,3,4,1,5] == [4]
+localMaxima [1,2,3,4,5] == []
+-}
+localMaxima :: [Integer] -> [Integer]
+localMaxima [] = []
+localMaxima lst@(x:y:z:_)
+  | (y > x) && (y > z) = y : localMaxima (tail lst)
+  | otherwise = localMaxima (tail lst)
+localMaxima lst = localMaxima (tail lst)
