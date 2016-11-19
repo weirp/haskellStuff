6b21a5596134ad6ee8d22aed0215d4c5e963d7dd
Philip J Weir <weirp@acm.org>
2016-11-20 07:03:08 +0800

    tidy up skips code

--- cis194/Golf.hs
+++ cis194/Golf.hs
@@ -7,11 +7,12 @@ given a list, take every element, then every 2nd element, every third, etc
 -}
 skips :: [a] -> [[a]]
 skips [] = [[]]
-skips all@(x:xs) = map (skipsHelper all) [1..(length all)]
+skips lst = map (skipsHelper lst) [1..(length lst)]
 
 
 skipsHelper :: [a] -> Int -> [a]
 skipsHelper [] _ = []
-skipsHelper all@(x:xs) n
-  | (length all) >= n = (head (take 1 (drop (n - 1) all))) : (skipsHelper (drop n all) n)
+skipsHelper lst n
+  | (length lst) >= n =
+      (head (take 1 (drop (n - 1) lst))) : (skipsHelper (drop n lst) n)
   | otherwise = []
