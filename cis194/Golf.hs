fc4de7a840c7579e7851719d61ecf22edd40e71e
Philip J Weir <weirp@acm.org>
2016-11-20 21:24:38 +0800

    incomplete, histogram; but broken

--- cis194/Golf.hs
+++ cis194/Golf.hs
@@ -39,3 +39,48 @@ localMaxima lst@(x:y:z:_)
   | (y > x) && (y > z) = y : localMaxima (tail lst)
   | otherwise = localMaxima (tail lst)
 localMaxima lst = localMaxima (tail lst)
+
+
+{-
+Exercise 3 Histogram
+For this task, write a function
+histogram :: [Integer] -> String
+which takes as input a list of Integers between 0 and 9 (inclusive),
+and outputs a vertical histogram showing how many of each number
+were in the input list. You may assume that the input list does not
+contain any numbers less than zero or greater than 9 (that is, it does
+not matter what your function does if the input does contain such
+numbers). Your output must exactly match the output shown in the
+examples below.
+cis 194: homework 3 4
+histogram [1,1,1,5] ==
+*
+*
+* *
+==========
+0123456789
+histogram [1,4,5,4,6,6,3,4,2,4,9] ==
+*
+*
+* *
+****** *
+==========
+0123456789
+Important note: If you type something like histogram [3,5] at
+the ghci prompt, you should see something like this:
+" * * \n==========\n0123456789\n"
+This is a textual representation of the String output, including \n
+escape sequences to indicate newline characters. To actually visualize
+the histogram as in the examples above, use putStr, for example,
+putStr (histogram [3,5]).
+-}
+
+
+histogram :: [Integer] -> String
+histogram [] = ""
+histogram lst =
+  let m = map (\x -> head x, length x) $ group $ sort lst
+  map (\x -> let l = lookup x m
+             case l of
+               Nothing = " "
+               Just x) [0..9]
