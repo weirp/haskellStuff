039724d7efa2f9b6ca329ef02b60cef377bcea6d
Philip J Weir <weirp@acm.org>
2016-12-03 23:42:41 +0800

    more on ex 5

--- cis194/Calc.hs
+++ cis194/Calc.hs
@@ -1,9 +1,15 @@
 {-# OPTIONS_GHC -Wall #-}
+{-# LANGUAGE DataKinds, KindSignatures #-}
+{-# LANGUAGE TypeSynonymInstances, FlexibleInstances #-}
+
 module Calc where
 
 import ExprT
 import Parser
-import Data.Maybe()
+import Data.Maybe
+
+import StackVM
+import qualified Data.Map as M
 
 {-
 Exercise 1
@@ -14,9 +20,9 @@ For example, eval (Mul (Add (Lit 2) (Lit 3)) (Lit 4)) == 20.
 -}
 
 eval :: ExprT -> Integer
-eval (Lit n) = n
-eval (Add left right) = (eval left) + (eval right)
-eval (Mul left right) = (eval left) * (eval right)
+eval (ExprT.Lit n) = n
+eval (ExprT.Add left right) = (eval left) + (eval right)
+eval (ExprT.Mul left right) = (eval left) * (eval right)
 
 
 {-
@@ -49,4 +55,132 @@ evalStr :: String -> Maybe Integer
 evalStr s = case x of
   Nothing -> Nothing
   Just n -> Just (eval n)
-  where x = parseExp Lit Add Mul s
+  where x = parseExp ExprT.Lit ExprT.Add ExprT.Mul s
+
+
+
+{-
+Exercise 3
+Good news! Early customer feedback indicates that people really
+do love the interface! Unfortunately, there seems to be some disagreement
+over exactly how the calculator should go about its calculating
+business. The problem the software department (i.e. you) has is that
+while ExprT is nice, it is also rather inflexible, which makes catering
+to diverse demographics a bit clumsy. You decide to abstract away
+the properties of ExprT with a type class.
+Create a type class called Expr with three methods called lit, add,
+and mul which parallel the constructors of ExprT. Make an instance of
+Expr for the ExprT type, in such a way that
+cis 194: homework 5 3
+mul (add (lit 2) (lit 3)) (lit 4) :: ExprT
+== Mul (Add (Lit 2) (Lit 3)) (Lit 4)
+Think carefully about what types lit, add, and mul should have. It
+may be helpful to consider the types of the ExprT constructors, which
+you can find out by typing (for example)
+*Calc> :t Lit
+at the ghci prompt.
+
+-}
+
+class Expr a where
+  lit :: Integer -> a
+  mul :: a -> a -> a
+  add :: a -> a -> a
+
+instance Expr ExprT where
+  lit = ExprT.Lit
+  add = ExprT.Add
+  mul = ExprT.Mul
+
+
+reify :: ExprT -> ExprT
+reify = id
+
+
+{-
+use like :::
+reify $ add (lit 2) (lit 3)
+
+this was mentioned in the question..... should have read it in full!!!
+
+-}
+
+
+
+{-
+Exercise 4
+The marketing department has gotten wind of just how flexible
+the calculator project is and has promised custom calculators to some
+big clients. As you noticed after the initial roll-out, everyone loves the
+interface, but everyone seems to have their own opinion on what the
+semantics should be. Remember when we wrote ExprT and thought
+that addition and multiplication of integers was pretty cut and dried?
+Well, it turns out that some big clients want customized calculators
+with behaviors that they have decided are right for them.
+The point of our Expr type class is that we can now write down
+arithmetic expressions once and have them interpreted in various
+ways just by using them at various types.
+cis 194: homework 5 4
+Make instances of Expr for each of the following types:
+• Integer — works like the original calculator
+• Bool — every literal value less than or equal to 0 is interpreted
+as False, and all positive Integers
+are interpreted as True; “addition” is logical or,
+“multiplication” is logical and
+• MinMax — “addition” is taken to be the max function, while
+“multiplication” is the min function
+• Mod7 — all values should be in the ranage 0 . . . 6, and
+all arithmetic is done modulo 7; for example,
+5 + 3 = 1.
+The last two variants work with Integers internally, but in order
+to provide different instances, we wrap those Integers in newtype
+wrappers. These are used just like the data constructors we’ve seen
+before.
+newtype MinMax = MinMax Integer deriving (Eq, Show)
+newtype Mod7 = Mod7 Integer deriving (Eq, Show)
+Once done, the following code should demonstrate our family of
+calculators:
+testExp :: Expr a => Maybe a
+testExp = parseExp lit add mul "(3 * -4) + 5"
+testInteger = testExp :: Maybe Integer
+testBool = testExp :: Maybe Bool
+testMM = testExp :: Maybe MinMax
+testSat = testExp :: Maybe Mod7
+Try printing out each of those tests in ghci to see if things are
+working. It’s great how easy it is for us to swap in new semantics for
+the same syntactic expression!
+-}
+
+instance Expr Integer where
+  lit = id
+  add = (+)
+  mul = (*)
+
+instance Expr Bool where
+  lit x
+    | x <= 0 = False
+    | otherwise = True
+  add = (||)
+  mul = (&&)
+
+newtype MinMax = MinMax Integer deriving (Eq, Show)
+
+instance Expr MinMax where
+  lit = MinMax
+  add (MinMax x) (MinMax y) = MinMax (max x y)
+  mul (MinMax x) (MinMax y) = MinMax (min x y)
+
+
+newtype Mod7 = Mod7 Integer deriving (Eq, Show)
+
+instance Expr Mod7 where
+  lit x = Mod7 (x `mod` 7)
+  add (Mod7  x) (Mod7 y) = Mod7 ((x + y) `mod` 7)
+  mul (Mod7 x) (Mod7 y) = Mod7 ((x * y) `mod` 7)
+
+testExp :: Expr a => Maybe a
+testExp = parseExp lit add mul "(3 * -4) + 5"
+testInteger = testExp :: Maybe Integer
+testBool = testExp :: Maybe Bool
+testMM = testExp :: Maybe MinMax
+testSat = testExp :: Maybe Mod7
