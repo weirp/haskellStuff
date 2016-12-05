b395aa12274883f9702df5a4bb62c58ecc37c026
Philip J Weir <weirp@acm.org>
2016-12-06 06:27:03 +0800

    just updating

--- cis194/Fibonacci.hs
+++ cis194/Fibonacci.hs
@@ -26,3 +26,41 @@ fibs2_helper2 (n1, n2) = (maximum [n1, n2], n1 + n2)
 
 fibs2 :: [Integer]
 fibs2 = map fst (iterate fibs2_helper2 (0,1))
+
+
+
+
+data Stream a = Cons a (Stream a)
+
+streamToList :: Stream a -> [a]
+streamToList (Cons a s) = a : streamToList s
+
+instance Show a => Show (Stream a) where
+  show = show . take 20 .streamToList
+
+
+streamRepeat :: a -> Stream a
+streamRepeat a = Cons a (streamRepeat a)
+
+streamMap :: (a -> b) -> Stream a -> Stream b
+streamMap f (Cons a s) = (Cons (f a)) (streamMap f s)
+
+
+streamFromSeed :: (a -> a) -> a -> Stream a
+streamFromSeed f a = Cons a (streamFromSeed f (f a))
+
+
+nats :: Stream Integer
+nats = streamFromSeed (+ 1) 0
+
+
+interleaveStreams :: Stream a -> Stream a -> Stream a
+interleaveStreams (Cons a as) (Cons b bs) = Cons a (Cons b (interleaveStreams as bs))
+
+{-
+interleaveStreams (streamFromSeed id 0) nats
+
+[2,4 .. 20]
+{-
+ruler :: Stream Integer
+-}
